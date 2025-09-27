5e5b7ed8543ab4ff3e7c6e69b6d29a8850a01f02
Tobias <tobiasvarela03@gmail.com>
2025-09-27 19:57:56 -0300

    Funcionalidad toolbar y menu

--- app/src/main/java/com/example/tp_grupol/Portfolio.kt
+++ app/src/main/java/com/example/tp_grupol/Portfolio.kt
@@ -1,12 +1,18 @@
 package com.example.tp_grupol
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Portfolio : AppCompatActivity() {
+    lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,22 @@ class Portfolio : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        toolbar= findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title=resources.getString(R.string.toolbar)
+        toolbar.overflowIcon = ContextCompat.getDrawable(this, R.drawable.baseline_work_outline_24)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_configuracion, menu)
+        menuInflater.inflate(R.menu.menu_transaccion, menu)
+        menuInflater.inflate(R.menu.menu_portafolio,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_configuracion){
+            val intent = Intent(this, Configuration::class.java)
+        startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
