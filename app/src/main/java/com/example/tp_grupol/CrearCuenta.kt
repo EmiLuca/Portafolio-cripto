23a07bda5a03a35d6700f4a49c4398e86495432a
Tobias <tobiasvarela03@gmail.com>
2025-09-30 20:46:47 -0300

    mergeo

--- app/src/main/java/com/example/tp_grupol/CrearCuenta.kt
+++ app/src/main/java/com/example/tp_grupol/CrearCuenta.kt
@@ -1,12 +1,24 @@
 package com.example.tp_grupol
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CrearCuenta : AppCompatActivity() {
+    lateinit var editName: EditText
+    lateinit var editEmail: EditText
+    lateinit var editPassword: EditText
+    lateinit var editConfirmPassword: EditText
+
+    lateinit var btnCreateAccount: Button
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +29,24 @@ class CrearCuenta : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        editName = findViewById(R.id.editName)
+        editEmail = findViewById(R.id.editEmail)
+        editPassword = findViewById(R.id.editPassword)
+        editConfirmPassword = findViewById(R.id.editConfirmPassword)
+        btnCreateAccount = findViewById(R.id.btnCreateAccount)
+
+        btnCreateAccount.setOnClickListener {
+            if(editPassword.text.toString().isEmpty()||editEmail.text.toString().isEmpty()||editName.text.toString().isEmpty()||editConfirmPassword.text.toString().isEmpty()){
+                Toast.makeText(this, "You must complete all fields.", Toast.LENGTH_SHORT).show()
+            }else{
+                val portfolio = Intent(this,Portfolio::class.java)
+                startActivity(portfolio)
+            }
+
+        }
+
     }
+
+
 }
