d181bc2fb66f44a63a1512ccd9a7e9fa75599b66
EmiLuca <emilianolucaledesma@gmail.com>
2025-10-01 12:13:51 -0300

    Mergeo 5

---
+++ app/src/main/java/com/example/tp_grupol/CoinAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.tp_grupol
+
+import Coins
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CoinAdapter (var coins: MutableList<Coins>, var context: Context) : RecyclerView.Adapter<CoinAdapter.CoinViewHolder>() {
+    class CoinViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val ivLogo: ImageView
+        val tvNombre: TextView
+        val tvSimCot: TextView
+        val tvPrecioActual: TextView
+        val tvTotal: TextView
+        val tvCantidad: TextView
+
+        init {
+            ivLogo = view.findViewById(R.id.ivLogo)
+            tvNombre = view.findViewById(R.id.tvNombre)
+            tvSimCot = view.findViewById(R.id.tvSimCot)
+            tvPrecioActual = view.findViewById(R.id.tvPrecioActual)
+            tvTotal = view.findViewById(R.id.tvTotal)
+            tvCantidad = view.findViewById(R.id.tvCantidad)
+        }
+    }
+    override fun getItemCount() = coins.size
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): CoinViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_coins, viewGroup, false)
+        return CoinViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CoinViewHolder, position: Int) {
+        val item = coins.get(position)
+        holder.tvNombre.text = item.nombre
+        holder.tvSimCot.text = item.simCot
+        holder.tvPrecioActual.text = "$${item.precioActual}"
+        holder.tvTotal.text = "$${item.precioActual * item.cambioPrecio24HS}"
+        holder.tvCantidad.text = item.cambioPrecio24HS.toString()
+    }
+
+    // Para refrescar la lista cuando llegan nuevos datos
+    fun updateData(newList: List<Coins>) {
+        coins.clear()
+        coins.addAll(newList)
+        notifyDataSetChanged()
+    }
+}
