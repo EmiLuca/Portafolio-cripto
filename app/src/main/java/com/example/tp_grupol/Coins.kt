b2fa717cd10749ceface6c0d97e40871d6fb380c
EmiLuca <emilianolucaledesma@gmail.com>
2025-10-19 19:49:50 -0300

    Agregado de dependencias, actualización de Java, adaptacion de data class coin y RecyclerView parar JSON

--- app/src/main/java/com/example/tp_grupol/Coins.kt
+++ app/src/main/java/com/example/tp_grupol/Coins.kt
@@ -1,13 +1,14 @@
 package com.example.tp_grupol
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import com.squareup.moshi.JsonClass
 
-@Entity(tableName = "coins")
+@JsonClass(generateAdapter = true)
 data class Coins(
-    @ColumnInfo(name = "Nombre") val nombre: String,
-    @ColumnInfo(name = "simbolo de cotizacion") val simCot: String,
-    @ColumnInfo(name = "icono") val icono: String,
-    @ColumnInfo(name = "precio") val precioActual: Double
-){@PrimaryKey(autoGenerate = true) var id: Int = 0}
+    var id: String,
+    var symbol: String,
+    var name: String,
+    var image: String,
+    var currentPrice: Int,
+    var marketCapRank: Int,
+    var marketCap: Long
+)
