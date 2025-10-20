ae6837357675557dbbd28532412974b5108b481c
EmiLuca <emilianolucaledesma@gmail.com>
2025-10-19 21:13:46 -0300

    Implementacion de la API y union con RecyclerView

---
+++ app/src/main/java/com/example/tp_grupol/ApiEndpoints.kt
@@ -0,0 +1,16 @@
+package com.example.tp_grupol
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiEndpoints {
+    @GET("coins/markets")
+    fun getCoins(
+        @Query("vs_currency") vsCurrency: String = "usd",
+        @Query("order") order: String = "market_cap_desc",
+        @Query("per_page") perPage: Int = 250,
+        @Query("page") page: Int = 1,
+        @Query("sparkline") sparkline: Boolean = false
+    ): Call<List<Coins>>
+}
